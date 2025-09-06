e512e39dcf14fe01ae3d915b3bbc06b3826276e8
co11aps <oleksandr.fedorov@gmail.com>
2025-09-06 18:28:32 +0800

    hero title animation and rotation  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import gsap from 'gsap';
 import NavBar from './components/NavBar';
 import HeroSection from './sections/HeroSection';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <div className="h-dvh border border-red-500"></div>
     </main>
   );
 };
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -31,6 +31,23 @@ const HeroSection = () => {
         },
         '-=0.5'
       );
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.hero-container',
+        start: '1% top',
+        end: 'bottom top',
+        scrub: true,
+        markers: false,
+      },
+    });
+
+    heroTl.to('.hero-container', {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: 'power1.inOut',
+    });
   });
 
   return (
